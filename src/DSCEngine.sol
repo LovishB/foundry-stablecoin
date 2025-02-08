0712ac405a51b94ff5a7651cf98bc8dd1d233af1
Lovish Badlani <lovish@dxsale.network>
2025-02-08 21:03:43 +0530

    Added minting and health factor

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -25,6 +25,14 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
  */
 contract DSCEngine is ReentrancyGuard {
 
+    //Constants
+    uint256 private constant MINIMUM_HEALTH_FACTOR = 1e18; // 1.0
+    uint256 private constant LIQUIDATION_THRESHOLD = 150; // 150%
+    uint256 private constant OPTIMAL_COLLATERAL_RATIO = 200; // 200%
+    uint256 private constant PERCENTAGE = 100; // 300%
+    uint256 private constant PRECISION = 1e18;
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+
     // Our stablecoin contract
     DecentralizedStableCoin private immutable i_dsc;
     // The ERC20 token contract for WETH collateral
@@ -33,11 +41,16 @@ contract DSCEngine is ReentrancyGuard {
     AggregatorV3Interface private immutable i_ethUsdPriceFeed;
     // Mapping for tracking collateral balances of users
     mapping(address user => uint256 amount) private s_collateralBalances;
+    // Mapping for tracking DSC balances of users
+    mapping(address user => uint256 amount) private s_dscBalances;
 
     //Errors
     error DSCEngine__AmountMustBeMoreThanZero();
     error DSCEngine__TokenNotAllowed();
-    error DSC_EngineTransferFailed();
+    error DSCEngine__TransferFailed();
+    error DSCEngine__HealthFactorBelowThreshold(uint256 healthFactor);
+    error DSCEngine__NotEnoughCollateral();
+    error DSCEngine__MintFailed();
 
     //Events
     event CollateralDeposited(address indexed user, address indexed token, uint256 amount);
@@ -71,12 +84,41 @@ contract DSCEngine is ReentrancyGuard {
         i_weth = IERC20(wethAddress);
     }
 
-    function depositCollateralAndMintDSC() external {}
+     /**
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
+     * @param amountCollateral: The amount of collateral you're depositing
+     * @param amountDscToMint: The amount of DSC you want to mint
+     * @notice This function will deposit your collateral and mint DSC in one transaction
+     */
+    function depositCollateralAndMintDsc(
+        address tokenCollateralAddress,
+        uint256 amountCollateral,
+        uint256 amountDscToMint
+    ) external {
+        depositCollateral(tokenCollateralAddress, amountCollateral);
+        mintDSC(amountDscToMint);
+    }
 
+    /**
+    * @notice Allows users to deposit WETH collateral to the protocol
+    * @dev The collateral is transferred from the user to this contract
+    * @dev Emits CollateralDeposited event on successful deposit
+    * @dev User must approve this contract to spend their WETH before calling
+    * 
+    * @param _collateralTokenAddress The address of the collateral token (must be WETH)
+    * @param _collateralAmount The amount of collateral to deposit (in wei)
+    * 
+    * Requirements:
+    * - Amount must be greater than 0
+    * - Token must be WETH
+    * - Transfer must succeed
+    * 
+    * Emits a {CollateralDeposited} event
+    */
     function depositCollateral(
         address _collateralTokenAddress,
         uint256 _collateralAmount
-    ) moreThanZero(_collateralAmount) isAllowedToken(_collateralTokenAddress) nonReentrant() external {
+    ) moreThanZero(_collateralAmount) isAllowedToken(_collateralTokenAddress) nonReentrant() public {
         // 1. Update the user's collateral balance in our tracking
         s_collateralBalances[msg.sender] += _collateralAmount;
 
@@ -87,7 +129,7 @@ contract DSCEngine is ReentrancyGuard {
         // Note: This contract must be approved to spend the collateral token
         bool success = IERC20(_collateralTokenAddress).transferFrom(msg.sender, address(this), _collateralAmount);
         if(!success) {
-            revert DSC_EngineTransferFailed();
+            revert DSCEngine__TransferFailed();
         }
     }
 
@@ -95,11 +137,110 @@ contract DSCEngine is ReentrancyGuard {
 
     function redeemCollateral() external {}
 
-    function mintDSC() external {
-    }
-
     function burnDSC() external {}
 
-    function getHealthFactor() external {}
+    function liquidate() external {}
+
+    /**
+    * @notice Mints DSC tokens for the caller based on their collateral
+    * @dev Enforces a 200% collateralization ratio for all mints
+    * @dev DSC balance is divided by PRECISION since 1 DSC = 1 USD
+    *
+    * @param _amountDscToMint Amount of DSC to mint (in wei)
+    * 
+    * Requirements:
+    * - Amount must be greater than 0
+    * - User must have enough collateral to mint DSC (200% collateral ratio)
+    * 
+    * Example:
+    * - User has $2000 worth of ETH collateral
+    * - Can mint up to 1000 DSC (200% collateral ratio)
+    * - If user already has 300 DSC, they can mint 700 more
+    */
+    function mintDSC(
+        uint256 _amountDscToMint
+    ) moreThanZero(_amountDscToMint) nonReentrant public {
+        // 1. Get the user's collateral balance in USD
+        uint256 collateralValue = _getCollateralValueInUsd(msg.sender);
+
+        // 2. Calculate total DSC amount post mint in USD
+        uint256 totalDscAmount = (s_dscBalances[msg.sender] + _amountDscToMint) / PRECISION;
+
+        // 3. Check if the user has enough collateral to mint DSC (200%)
+        if(collateralValue < (totalDscAmount * OPTIMAL_COLLATERAL_RATIO) / PERCENTAGE) {
+            revert DSCEngine__NotEnoughCollateral();
+        }
+
+        // 4. Update the user's DSC balance in our tracking
+        s_dscBalances[msg.sender] += _amountDscToMint;
+
+        // 5. Mint the DSC tokens to the user
+        bool success = i_dsc.mint(msg.sender, _amountDscToMint);
+        if(!success) {
+            revert DSCEngine__MintFailed();
+        }
+    }
+
+    /**
+    * @notice Reverts if the health factor of a user's position is below the threshold
+    * @param user The address of the user to check
+    */
+    function _revertIfHealthFactorBelowThreshold(address user) private view {
+        uint256 healthFactor = _healthFactor(user);
+        if(healthFactor < MINIMUM_HEALTH_FACTOR) {
+            revert DSCEngine__HealthFactorBelowThreshold(healthFactor);
+        }
+    }
+
+    /**
+    * @notice Calculates the health factor for a user's position
+    * @dev Health factor is the ratio of collateral value to loan value
+    * @dev Formula: (collateralValueInUsd * 100) / (dscBalance * 150)
+    * @dev DSC balance is divided by PRECISION since 1 DSC = 1 USD
+    * @dev _getCollateralValueInUsd returns the USD value of collateral
+    *
+    * @param user The address of the user to check
+    * 
+    * Example scenarios (assuming PERCENTAGE = 100, LIQUIDATION_THRESHOLD = 150):
+    * - At 150% collateral: returns 1.0 (liquidation point)
+    * - At 200% collateral: returns 1.33 (healthy)
+    * - At 100% collateral: returns 0.66 (unhealthy)
+    */
+    function _healthFactor(address user) private view returns (uint256) {
+        // 1. Get total DSC balance of the user (1DSC = 1USD)
+        uint256 dscBalance = s_dscBalances[user] / PRECISION;
+        if(dscBalance == 0) {
+            return 0;
+        }
+        // 2. Get total collateral balance of the user (in USD)
+        uint256 collateralBalance = _getCollateralValueInUsd(user);
+
+        // 3. Calculate the health factor
+        return collateralBalance * PERCENTAGE / (dscBalance * LIQUIDATION_THRESHOLD);
+    }
+
+    /**
+    * @notice Converts WETH collateral to USD value using Chainlink price feed
+    *
+    * @param user The address of the user whose collateral value to check
+    * @return uint256 The USD value of the user's collateral
+    * 
+    * Price conversion process:
+    * 1. Get collateral balance in WEI (18 decimals)
+    * 2. Get ETH/USD price from Chainlink (8 decimals)
+    * 3. Add ADDITIONAL_FEED_PRECISION (10 decimals) for better precision
+    * 4. Divide by PRECISION (18 decimals) for final USD value
+    */
+    function _getCollateralValueInUsd(address user) private view returns (uint256) {
+        // 1. Get the user's collateral balance (in WEI)
+        uint256 collateralBalance = s_collateralBalances[user];
+
+        // 2. Get the price of 1 WETH in USD
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(i_ethUsdPriceFeed);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+
+        // 3. Convert the collateral balance to USD value
+        return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * collateralBalance) / PRECISION;
+    }
 
 }
